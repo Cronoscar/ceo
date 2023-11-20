a655f17c2b012a2273180ec28b1ee2514f536249
Cronoscar <eucedao662@gmail.com>
2023-11-20 17:15:13 -0600

    inicio

---
+++ src/test/java/com/carwash/ceo/CeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carwash.ceo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
